628a7b55198e55c67d46bb3b475e551ba01899c3
1gig0 <1gig0@1gig0s-MacBook-Air.local>
2022-02-02 19:20:25 +0400

    rendering

--- subjects/01-Rendering/exercise.js
+++ subjects/01-Rendering/exercise.js
@@ -18,21 +18,31 @@ import ReactDOM from "react-dom";
 import sortBy from "sort-by";
 
 const DATA = {
-  title: "Menu",
-  items: [
-    { id: 1, name: "tacos", type: "mexican" },
-    { id: 2, name: "burrito", type: "mexican" },
-    { id: 3, name: "tostada", type: "mexican" },
-    { id: 4, name: "mushy peas", type: "english" },
-    { id: 5, name: "fish and chips", type: "english" },
-    { id: 6, name: "black pudding", type: "english" }
-  ]
+    title: "Menu",
+    items: [
+        {id: 1, name: "tacos", type: "mexican"},
+        {id: 2, name: "burrito", type: "mexican"},
+        {id: 3, name: "tostada", type: "mexican"},
+        {id: 4, name: "mushy peas", type: "english"},
+        {id: 5, name: "fish and chips", type: "english"},
+        {id: 6, name: "black pudding", type: "english"}
+    ]
 };
 
 function Menu() {
-  return <div>Open the console, you have failing tests.</div>;
+    const items = DATA.items
+        .filter(item => item.type === 'mexican')
+        .sort(sortBy('name'))
+        .map(item => <li key={item.id}>{item.name}</li>);
+
+    return (
+        <div>
+            {DATA.title && <h1>{DATA.title}</h1>}
+            {DATA.items && <ul>{items}</ul>}
+        </div>
+    );
 }
 
-ReactDOM.render(<Menu />, document.getElementById("app"));
+ReactDOM.render(<Menu/>, document.getElementById("app"));
 
 require("./tests").run();
