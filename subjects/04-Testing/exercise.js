9605bab0d0fa4e2eedfa9fe7c89f7f117db6de10
1gig0 <gig.sagrishvili@gmail.com>
2022-02-03 19:35:24 +0400

    feat(testing): add mocha testing to taps component

--- subjects/04-Testing/exercise.js
+++ subjects/04-Testing/exercise.js
@@ -14,38 +14,60 @@ import data from "./data";
 import Tabs from "./components/Tabs";
 
 describe("when <Tabs> is rendered", () => {
-  let node;
+  let node, tabs, activeTab;
+
+  const checkTapActivation = (index, isActive) => {
+    if(isActive) {
+      expect(tabs[index].style.borderBottomColor).toEqual(activeTab.style.borderBottomColor);
+    } else {
+      expect(tabs[index].style.borderBottomColor).not.toEqual(activeTab.style.borderBottomColor);
+    }
+  }
+
   beforeEach(() => {
     node = document.createElement("div");
     ReactDOM.render(<Tabs data={data} />, node);
+
+    tabs = node.querySelectorAll(".Tab");
+
+    activeTab = document.createElement("div");
+    activeTab.setAttribute("style", "border-bottom-color: #000");
   });
 
   afterEach(() => {
     ReactDOM.unmountComponentAtNode(node);
   });
 
-  it("renders the first tab", () => {
-    const tabs = node.querySelectorAll(".Tab");
-    expect(tabs[0].innerText).toEqual(data[0].name);
+  it("renders all tabs", () => {
+    tabs.forEach((tab, index) => {
+      expect(tab.innerText).toEqual(data[index].name);
+    })
+  })
+
+  it("activates the first tab", () => {
+    checkTapActivation(0, true);
   });
 
-  it("renders the second tab");
-
-  it("renders the third tab");
-
-  it("activates the first tab");
-
-  it("does not activate the second tab");
+  it("does not activate the second tab", () => {
+    checkTapActivation(1, false);
+  });
 
   describe("after clicking the second tab", () => {
     beforeEach(() => {
-      // TODO: simulate a click on the second tab
+      Simulate.click(tabs[1]);
     });
 
-    it("activates the second tab");
+    it("activates the second tab", () => {
+      checkTapActivation(1, true);
+    });
 
-    it("deactivates the first tab");
+    it("deactivates the first tab", () => {
+      checkTapActivation(0, false);
+    });
 
-    it("puts the correct content in the panel");
+    it("puts the correct content in the panel", () => {
+      const tabPanel = node.querySelector('.TabPanel');
+      expect(tabPanel.innerText).toEqual(data[1].description);
+    });
   });
 });
