c7ea1513b7936f2356cfb59ab8c6805399054cfb
1gig0 <gig.sagrishvili@gmail.com>
2022-02-04 23:54:27 +0400

    feat(HOC): add withMouse HOC

--- subjects/07-Higher-Order-Components/exercise.js
+++ subjects/07-Higher-Order-Components/exercise.js
@@ -14,25 +14,32 @@ import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import { render } from "less";
 
 function withMouse(Component) {
-  // TODO: Return a *new* component class that renders
-  // the given Component with a `mouse` prop
-  return Component;
+  return class extends React.Component {
+    state = { x: 0, y: 0 };
+
+    handleMouseMove = event => {
+      this.setState({ x: event.clientX, y: event.clientY });
+    };
+
+    render() {
+      return (
+        <div onMouseMove={this.handleMouseMove}>
+          <Component mouse={this.state} {...this.props} />
+        </div>
+      );
+    }
+
+  }
 }
 
 class App extends React.Component {
-  state = { x: 0, y: 0 };
-
-  handleMouseMove = event => {
-    this.setState({ x: event.clientX, y: event.clientY });
-  };
-
   render() {
-    const { x, y } = this.state;
-
+    const { x, y } = this.props.mouse;
     return (
-      <div className="container" onMouseMove={this.handleMouseMove}>
+      <div className="container">
         <h1>
           The mouse position is ({x}, {y})
         </h1>
